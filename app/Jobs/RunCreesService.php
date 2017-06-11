6f48a98f919bced62456d55903c07a988908c8e7
will <will.doran@riseup.net>
2017-06-10 22:32:42 -0400

    remove logging

--- app/Jobs/RunCreesService.php
+++ app/Jobs/RunCreesService.php
@@ -77,7 +77,6 @@ class RunCreesService extends RunProxyService
         // At the moment it is important to only set fields that you intend to change
         // as Post updates are async it is possible to overwrite other user's data
         // by performing a full update with the complete object recevied
-        Log::error(print_r($post, true));
         return [
             'id' => $post['id'],
             'tags' => $tags,
